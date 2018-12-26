4232b8ccb1b1825a9eac1b4edf09b7da1dd15978
AnonymousAllIndonesia <45931771+AnonymousAllIndonesia@users.noreply.github.com>
2018-12-27 00:12:52 +0700

    Update __init__.py

--- xlpy/__init__.py
+++ xlpy/__init__.py
@@ -5,6 +5,6 @@ __copyright__       = 'Copyright 2018 by Mr.Kintil'
 __version__         = '5.0.0'
 __author__          = 'Mr.Kintil'
 __author_email__    = 'aliattacker12@gmail.com'
-__url__             = 'https://github.com/ANONYMOUSALLINDONESIA/tembaktembakan.git'
+__url__             = 'https://github.com/AnonymousAllIndonesia/tembaktembakan.git'
 
 __all__ = ['Config','XL']
